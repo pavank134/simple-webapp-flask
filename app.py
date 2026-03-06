ee298780d068c6d6e8a177d1bad8a3787134ec7a
Your Name <you@example.com>
2026-03-06 15:35:11 +0530

    Add about route to Flask application

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route('/about')
+def about():
+    return 'This is a simple Flask web application'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
